223c429b1ed0f83556f8ff39fff7addc03855230
Andrew M <136806756+andrewmcs@users.noreply.github.com>
2024-11-21 18:14:11 +0000

    report the day and get the next weekday

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import { useState } from "react";
 export default function App() {
 
   const [day, setDay] = useState("Monday");
-
+  const getNextDay = () => {
+    if (day === "Monday") {
+      setDay("Tuesday")
+    } else if (day === "Tuesday") {
+      setDay("Wednesday")
+    } else if (day === "Wednesday") {
+      setDay("Thursday")
+    } else if (day === "Thursday") {
+      setDay("Friday")
+    } else if (day === "Friday") {
+      setDay("Monday")
+    }
+  }
   return (
     <div className="App">
       <h1>Today is {day}</h1>
+      <button onClick={getNextDay}>Get next day</button>
     </div>
   );
 }
 
+function usePrevious(val) { }
